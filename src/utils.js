a7b14ab8a4304e3d8d8c932fafd8be9db1b7524a
NarminGale <narmingale@gmail.com>
2021-06-28 23:50:39 +0400

    Exported urls

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,11 @@
+const allProductsUrl = 'https://course-api.com/javascript-store-products'
+const singleProductUrl =
+  'https://course-api.com/javascript-store-single-product'
+
 const getElement = (selection) => {
   const element = document.querySelector(selection)
   if (element) return element
   throw Error(`Please check "${selection}" selector, no such element exist`)
 }
+
+export { allProductsUrl, singleProductUrl, getElement }
