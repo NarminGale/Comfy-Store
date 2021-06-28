35f1d6600d41588464e29998db9496b677f13349
NarminGale <narmingale@gmail.com>
2021-06-28 23:49:23 +0400

    Imported fetchProducts.js

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ import './src/toggleSidebar.js'
 import './src/cart/toggleCart.js'
 
 // specific imports
+import fetchProducts from './src/fetchProducts.js'
+
+const init = async () => {
+  const products = await fetchProducts()
+}
+
+window.addEventListener('DOMContentLoaded', init)
