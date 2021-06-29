a40be3fc0621a16eb7292785a8648ec2d11c6fd8
NarminGale <narmingale@gmail.com>
2021-06-29 16:29:14 +0400

    Imported getStorageItem and setStorageItem js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,16 @@
-import { getStorageItem, setStorageItem } from './utils.js'
-let store = []
+import { setStorageItem } from './utils.js'
+
+const getStorageItem = (item) => {
+  let storageItem = localStorage.getItem(item)
+  if (storageItem) {
+    storageItem = JSON.parse(localStorage.getItem(item))
+  } else {
+    storageItem = []
+  }
+  return storageItem
+}
+
+let store = getStorageItem('store')
 const setupStore = (products) => {
   store = products.map((product) => {
     const {
@@ -10,7 +21,9 @@ const setupStore = (products) => {
     const image = img[0].thumbnails.large.url
     return { id, featured, name, price, company, colors, image }
   })
+  setStorageItem('store', store)
 }
+console.log(store)
 const findProduct = () => {}
 
 export { store, setupStore, findProduct }
