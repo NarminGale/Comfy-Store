143b279e8261dd34c6f0ccb0b7963ca4aebbab29
NarminGale <narmingale@gmail.com>
2021-06-30 16:33:51 +0400

    Created add to cart function

---
+++ src/cart/setupCart.js
@@ -0,0 +1,9 @@
+// import
+import { setStorageItem, formatPrice, getElement } from '../utils.js'
+import { openCart } from './toggleCart.js'
+
+// set items
+export const addToCart = (id) => {
+  console.log(id)
+  openCart()
+}
