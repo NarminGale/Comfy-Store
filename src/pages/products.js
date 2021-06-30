0d410e102dbd11733ee5dbfe8e8f8ce9b57b088f
NarminGale <narmingale@gmail.com>
2021-06-30 17:44:43 +0400

    Added page loading

--- src/pages/products.js
+++ src/pages/products.js
@@ -3,4 +3,8 @@ import { getElement } from '../utils.js'
 import display from '../displayProducts.js'
 import { store } from '../store.js'
 
+const loading = getElement('.page-loading')
+
 display(store, getElement('.products-container'))
+
+loading.style.display = 'none'
