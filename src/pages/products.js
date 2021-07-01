35272677ba625faaeb1cbf84ff2dc42938cbf504
NarminGale <narmingale@gmail.com>
2021-07-01 16:39:58 +0400

    Imported search.js

--- src/pages/products.js
+++ src/pages/products.js
@@ -3,8 +3,13 @@ import { getElement } from '../utils.js'
 import display from '../displayProducts.js'
 import { store } from '../store.js'
 
+// filter imports
+import setupSearch from '../filters/search.js'
+// import setupCompanies from '../filters/companies.js'
+// import setupPrice from '../filters/price.js'
+
 const loading = getElement('.page-loading')
 
 display(store, getElement('.products-container'))
-
+setupSearch(store)
 loading.style.display = 'none'
