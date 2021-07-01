73d8518a22298cd77b6723c32af3900139d075ae
NarminGale <narmingale@gmail.com>
2021-07-01 17:42:28 +0400

    Imported companies.js

--- src/pages/products.js
+++ src/pages/products.js
@@ -5,11 +5,12 @@ import { store } from '../store.js'
 
 // filter imports
 import setupSearch from '../filters/search.js'
-// import setupCompanies from '../filters/companies.js'
+import setupCompanies from '../filters/companies.js'
 // import setupPrice from '../filters/price.js'
 
 const loading = getElement('.page-loading')
 
 display(store, getElement('.products-container'))
 setupSearch(store)
+setupCompanies(store)
 loading.style.display = 'none'
